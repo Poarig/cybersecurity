1d6101ae5c0542bb4fcc9a27027cbec442d74e3d
Poarig <poarig@yandex.ru>
2023-12-18 20:45:05 +0300

    изменение настроек

--- cybersecurity/settings.py
+++ cybersecurity/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-1mek2!ko3e(27&uq$n2jpa%)hk+#%9jwfk(=a18!x4=v8@chwh'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
